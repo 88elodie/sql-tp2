464dafb1093e4848501a12aeefbea228676e92cc
Élodie <83143421+88elodie@users.noreply.github.com>
2022-09-15 22:31:41 -0400

    add order by desc post_date

--- mvc/models/forum.php
+++ mvc/models/forum.php
@@ -18,7 +18,7 @@ function forum_model_view($request){
         $$key=mysqli_real_escape_string($con,$value);
     }
     $sql = "SELECT title, forum_content, post_date, author_id, user_id, name FROM forum
-LEFT OUTER JOIN user ON forum.author_id = user.user_id";
+LEFT OUTER JOIN user ON forum.author_id = user.user_id ORDER BY post_date DESC";
     $result = mysqli_query($con, $sql);
     $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
     mysqli_close($con);
@@ -31,7 +31,7 @@ function forum_model_userview($request){
         $$key=mysqli_real_escape_string($con,$value);
     }
     $user_id = $_SESSION['user_id'];
-    $sql = "SELECT * FROM forum WHERE author_id = '$user_id'";
+    $sql = "SELECT * FROM forum WHERE author_id = '$user_id' ORDER BY post_date DESC";
     $result = mysqli_query($con, $sql);
     $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
     mysqli_close($con);
